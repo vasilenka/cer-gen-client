af90ac740fa585d8462a86eafc7dd4cf12f145c5
Ongki Herlambang <meepomodeon@gmail.com>
2019-04-22 09:57:26 +0700

    Updates

---
+++ precache-manifest.6039ef5e90496d6ceb42f736e36ebe75.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e05417efe8bc7e9332b",
+    "url": "/cer-gen-client/static/css/main.e22ce393.chunk.css"
+  },
+  {
+    "revision": "7e05417efe8bc7e9332b",
+    "url": "/cer-gen-client/static/js/main.7e05417e.chunk.js"
+  },
+  {
+    "revision": "31bcf5e30773c41eafd7",
+    "url": "/cer-gen-client/static/js/1.31bcf5e3.chunk.js"
+  },
+  {
+    "revision": "f9dff693fb96f3260ef6",
+    "url": "/cer-gen-client/static/js/runtime~main.f9dff693.js"
+  },
+  {
+    "revision": "a98fcb497937093b5460497307123f72",
+    "url": "/cer-gen-client/static/media/template.a98fcb49.jpg"
+  },
+  {
+    "revision": "a1d65c5b1d1547a34e3e54f964d8f4cd",
+    "url": "/cer-gen-client/static/media/Sentinel-Medium.a1d65c5b.otf"
+  },
+  {
+    "revision": "40c69937bc2b5f11d3ed6f64b3692bf6",
+    "url": "/cer-gen-client/static/media/Sentinel-Light.40c69937.otf"
+  },
+  {
+    "revision": "1ba5c38105e4f4c90d168cbef13114ab",
+    "url": "/cer-gen-client/static/media/Sentinel-BlackItalic.1ba5c381.otf"
+  },
+  {
+    "revision": "12bf326c0f4fea5ff2a6b06f88f08ace",
+    "url": "/cer-gen-client/static/media/Sentinel-Book.12bf326c.otf"
+  },
+  {
+    "revision": "2ccdefbdae1a3c49c0cfb3e848040b6a",
+    "url": "/cer-gen-client/static/media/Sentinel-Black.2ccdefbd.otf"
+  },
+  {
+    "revision": "49e282b7916a39fafac04ac4d4c397d2",
+    "url": "/cer-gen-client/static/media/Sentinel-MediumItalic.49e282b7.otf"
+  },
+  {
+    "revision": "a83d81a7774d0a128047ef2294d66703",
+    "url": "/cer-gen-client/static/media/Sentinel-Semibold.a83d81a7.otf"
+  },
+  {
+    "revision": "52d2cb71b40929e5263bcd113536133a",
+    "url": "/cer-gen-client/static/media/Sentinel-Bold.52d2cb71.otf"
+  },
+  {
+    "revision": "e999b6851f1cef1dae8690b5d87fc8f0",
+    "url": "/cer-gen-client/static/media/Sentinel-LightItalic.e999b685.otf"
+  },
+  {
+    "revision": "46e4ca8897ba13d1467632ef1a475314",
+    "url": "/cer-gen-client/static/media/Sentinel-SemiboldItalic.46e4ca88.otf"
+  },
+  {
+    "revision": "251a7d2bf4b30fd63941c04d20150984",
+    "url": "/cer-gen-client/static/media/Sentinel-BoldItalic.251a7d2b.otf"
+  },
+  {
+    "revision": "e554c4737726b7cc5ad5d1a6a3678515",
+    "url": "/cer-gen-client/static/media/Sentinel-BookItalic.e554c473.otf"
+  },
+  {
+    "revision": "e87561f832dc7a9d1a2d8e1145359dc3",
+    "url": "/cer-gen-client/static/media/Masbul-ExtraBold.e87561f8.ttf"
+  },
+  {
+    "revision": "459275d7d75dd5b0e83df41348e6b514",
+    "url": "/cer-gen-client/static/media/Masbul-Black.459275d7.ttf"
+  },
+  {
+    "revision": "ddc01b174e0cc44bc0802d69f902f5fa",
+    "url": "/cer-gen-client/static/media/Masbul-Bold.ddc01b17.ttf"
+  },
+  {
+    "revision": "d38557f85a2941b2492d8c3e89a943ef",
+    "url": "/cer-gen-client/static/media/Masbul-Medium.d38557f8.ttf"
+  },
+  {
+    "revision": "c24df600a03e9416e359cfb1cf5376a0",
+    "url": "/cer-gen-client/static/media/Masbul-Book.c24df600.ttf"
+  },
+  {
+    "revision": "a2f545d30dfa6ce81b53c5f8bab07d32",
+    "url": "/cer-gen-client/static/media/Masbul-Light.a2f545d3.ttf"
+  },
+  {
+    "revision": "ac1adccdd5f857da89d5c6203c799737",
+    "url": "/cer-gen-client/index.html"
+  }
+];
